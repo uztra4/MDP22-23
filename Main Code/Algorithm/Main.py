92995741a0f9018ce632c2e1de886f1d87ca6e7e
jonathanc1699 <70562873+jonathanc1699@users.noreply.github.com>
2022-09-05 18:38:07 +0800

    commit2

--- Main Code/Algorithm/Main.py
+++
@@ -1,6 +0,0 @@
-from Map import *
-
-maze = Map(20,2,19,0)
-maze.printMap()
-maze.moveCar(2)
-maze.printMap()
--- Main Code/Algorithm/client.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("172.21.145.192", 1234))
-
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
