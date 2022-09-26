57470e4ac6f760ce5eae7a83e2221a55fbb4a05e
brandonlokemk <brandonlokemk@gmail.com>
2022-09-26 16:32:24 +0800

    Added guidelines to the grid UI and shifted elements

--- Main Code/Android/MDP-NEW/app/src/main/java/com/example/mdp_new/Grid.java
+++ Main Code/Android/MDP-NEW/app/src/main/java/com/example/mdp_new/Grid.java
@@ -14,14 +14,14 @@ public class Grid extends View {
     private static final int cellHeight = 40;
 
     private static final Paint blackPaint = new Paint();
-    private static final Paint greyPaint = new Paint();
+    private static final Paint gridBackground = new Paint();
     private static final Paint whitePaint = new Paint();
 
     public Grid(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
 
         blackPaint.setColor(Color.BLACK);
-        greyPaint.setColor(Color.LTGRAY);
+        gridBackground.setColor(Color.WHITE);
         whitePaint.setColor(Color.WHITE);
     }
 
@@ -33,7 +33,7 @@ public class Grid extends View {
             for (int j = 0; j < numberOfRows; j++) {
                 canvas.drawRect(i * cellWidth, j * cellHeight,
                         (i + 1) * cellWidth, (j + 1) * cellHeight,
-                        greyPaint);
+                        gridBackground);
             }
         }
 
